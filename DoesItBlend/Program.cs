7072c5f2802cd5162d318edfdb0691f9091a6516
Samuel Brunner <sbrunner5124@ksu.edu>
2019-09-20 14:29:21 -0500

    DoesItBlend finished

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -6,7 +7,24 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendables = new List<IBlendable>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            Banana b = new Banana();
+            Console.WriteLine(b.Blend());
+            blendables.Add(b);
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            for (int i = 0; i < 10; i++)
+                blendables.Add(new Mango());
+
+            string mess = "";
+
+            foreach(IBlendable blendable in blendables)
+            {
+                mess += blendable.Blend();
+            }
+            Console.WriteLine(mess);
         }
     }
 }
